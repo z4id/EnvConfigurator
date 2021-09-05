3089a2a30cb19e3657bd4a148fe170bfa42347ed
Zaid Afzal <zaidafzal@protonmail.com>
2021-09-05 17:29:55 +0500

    New Release: add files for alpha v0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name='EnvConfigurator',
+  packages=['EnvConfigurator'],
+  version='0.1',
+  license='MIT',
+  description='Load and Parse Environment Variable(s) with just 3 lines of code.',
+  author='Muhammad Zaid Afza',
+  author_email='zaidafzal@@protonmail.com',
+  url='https://github.com/z4id',
+  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords=['ENVIRONMENT', 'CONFIGURATION', 'VARIABLE'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
